f98619b9d68be46c090ddcea172efc8df79853c0
ruthbhatti <ruthbhatti55@gmail.com>
2024-05-16 21:35:50 -0400

    made the comments work

--- comments.js
+++ comments.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const commentsList = document.getElementById('commentsList');
 
     const comments = [
-        { name: 'Victor Pinto', date: '11/02/2023', text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
         { name: 'Christina Cabrera', date: '10/28/2023', text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' },
         { name: 'Isaac Tadesse', date: '10/20/2023', text: 'I can\'t stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can\'t get enough.' }
     ];
